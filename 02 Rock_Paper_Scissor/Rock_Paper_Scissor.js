e9d89d1759dc8419fc2018a7aee4e26a362707b9
Jeremy Shen <jeremyshen1987@gmail.com>
2022-03-13 21:56:31 -0600

    wrote the function play a 5 round game that keeps score and reports a winner or loser at the end.  Project complete

--- 02 Rock_Paper_Scissor/Rock_Paper_Scissor.js
+++ 02 Rock_Paper_Scissor/Rock_Paper_Scissor.js
@@ -7,11 +7,6 @@ function computer_play(){
 
 
 
-const player_pick = prompt('rock, paper or scissors ?').toLowerCase();
-const computer_pick = computer_play();
-
-console.log(computer_pick);
-
 function single_round(player_pick,computer_pick){
 
 
@@ -20,20 +15,58 @@ function single_round(player_pick,computer_pick){
     else {
 
         switch(player_pick){
-            case "rock": computer_pick=="scissors" ? console.log('rock beats scissors. you win') :
-                console.log('paper beats rock, you lose!')
+            case "rock": computer_pick=="scissors" ? (console.log('rock beats scissors. you win'), player_score+=1) :
+                (console.log('paper beats rock, you lose!'), computer_score+=1)
             break;
             
-            case "scissor": computer_pick=="paper" ? console.log('scisor beats paper. you win') :
-                console.log('rock beats scissor, you lose!')
+            case "scissor": computer_pick=="paper" ? (console.log('scisor beats paper. you win'), player_score+=1) :
+                (console.log('rock beats scissor, you lose!'), computer_score+=1)
             break;
 
-            case "paper": computer_pick=="rock" ? console.log('paper beats rock, you win') :
-                console.log('scissor beats paper, you lose!')
+            case "paper": computer_pick=="rock" ? (console.log('paper beats rock, you win'), player_score+=1) :
+                (console.log('scissor beats paper, you lose!'), computer_score+=1)
             break;
         }
     }
     
 }
 
-single_round(player_pick,computer_pick);
+
+let player_score = 0,
+    computer_score = 0;
+
+
+function main(){
+
+    for(let i = 0; i < 5; i++){
+
+
+        let player_pick = prompt('rock, paper or scissors ?').toLowerCase();
+        const computer_pick = computer_play();
+
+        while(true){
+
+            if (player_pick !== 'rock' && player_pick !== 'paper' && player_pick !== 'scissors'){
+                player_pick = prompt('invalid input! select one of them: rock, paper or scissors')
+            }
+            else{
+                break;
+            }
+        }
+
+        console.log(computer_pick);
+
+        single_round(player_pick,computer_pick);
+
+        console.log('player score: ' + player_score);
+        console.log('computer score: ' + computer_score);
+
+    };
+
+(player_score > computer_score) ? console.log(`Game Over, you win! \n final score: Player:${player_score} Computer:${computer_score}`) :
+(player_score < computer_score) ? console.log(`Game Over, Computer win! \n final score: Player:${player_score} Computer:${computer_score}`) :
+console.log(`Game Over, Draw! \n final score: Player:${player_score} Computer:${computer_score}`);
+
+}
+
+main();
