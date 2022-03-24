30f1bc6b1531cf3f8edd3aecd888897b8c679af4
Jeremy Shen <jeremyshen1987@gmail.com>
2022-03-23 23:04:57 -0600

    add function "Modify_Box_Num(x)" to change num of boxes per side, available in console only

--- 03 ETCH-A-SKETCH/ETCH-A-SKETCH.js
+++ 03 ETCH-A-SKETCH/ETCH-A-SKETCH.js
@@ -6,27 +6,83 @@ box.classList.add('box')
 // box.innerHTML = 'haha'
 
 
-for (i = 1; i <= 256; i++){
+function createBoxes(Box_Num){
 
-    container.appendChild(box.cloneNode(true))
+    for (i = 0; i < Box_Num; i++){
+
+        container.appendChild(box.cloneNode(true))
+    
+    }
 
 }
 
-function effect(){
+createBoxes(256)
+
+
+function draw(){
 
     this.classList.add('sketching')
 
 }
 
-function removeClass(){
+function erase(){
 
     this.classList.remove('sketching')
-    
+
 }
 
 
 const boxes = document.querySelectorAll('.box')
-boxes.forEach(item => item.addEventListener('mouseover', effect))
+boxes.forEach(item => item.addEventListener('mouseover', draw))
+
+boxes.forEach(item => item.addEventListener('transitionend', erase))
 
 
-boxes.forEach(item => item.addEventListener('transitionend', removeClass))
+
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+
+function Modify_Box_Num(Num_Boxes_Side){
+
+
+    removeElementsByClass('box')
+
+
+    Boxes_Total = Math.pow(Num_Boxes_Side,2)
+    
+    Area_Per_Box = 640000 / Boxes_Total
+    
+    Width_Box = Math.pow(Area_Per_Box,0.5)
+
+    
+    createBoxes(Boxes_Total)
+
+    
+    new_boxes = document.querySelectorAll('.box')
+
+    
+    for(i = 0; i < Boxes_Total; i++){
+
+        new_boxes[i].style.flex = '1 1 auto'
+        new_boxes[i].style.width = `${Width_Box}px`
+        new_boxes[i].style.height = `${Width_Box}px`
+    
+    }
+
+    new_boxes.forEach(item => item.addEventListener('mouseover', draw))
+
+    new_boxes.forEach(item => item.addEventListener('transitionend', erase))
+
+
+
+}
+
+
+
+
+
