3cb3409effe01fbe48a684ab5a017c3731e2f487
Jeremy Shen <jeremyshen1987@gmail.com>
2022-03-26 15:42:11 -0600

    add calculator interface, +-*/ functions

---
+++ 04 Calculator/logic.js
@@ -0,0 +1,22 @@
+function add(a,b){
+    return a + b
+}
+
+function sub(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+
+
+function main (operator,num1,num2) {
+
+
+}
