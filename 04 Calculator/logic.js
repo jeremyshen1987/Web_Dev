48c49a436c4c7ff2f1290686e260f319ac969ef5
Jeremy Shen <jeremyshen1987@gmail.com>
2022-03-27 11:44:47 -0600

    chained calculation complete

--- 04 Calculator/logic.js
+++ 04 Calculator/logic.js
@@ -15,8 +15,107 @@ function divide(a,b){
 }
 
 
+screenLwr = document.querySelector('.screenLwr')
+screenUpr = document.querySelector('.screenUppr')
+Num_Buttons = document.querySelectorAll('.numbers')
+Operator_Buttons = document.querySelectorAll('.operators')
 
-function main (operator,num1,num2) {
+let Num1
+let Num2
+let First_Operator
+let Second_Operator
 
+OperatorReady = false
+ClearScreen = false
+
+function AddNum(Num){
+
+    console.log('run')
+
+    if(ClearScreen){
+        screenLwr.textContent = ''
+        ClearScreen = false
+    }
+
+    screenLwr.textContent += Num
+
+    
 
 }
+
+function GetOperator(Operator){
+
+    if(ClearScreen){
+        return
+    }
+    
+    if(OperatorReady){
+
+        Assessment(Operator)
+
+    }
+
+    if(Operator != '='){
+        First_Operator = Operator
+        OperatorReady = true
+    }
+
+
+    
+    Num1 = parseFloat(screenLwr.textContent)
+    ClearScreen = true
+    console.log('end of GetOperator')
+
+}
+
+function Assessment(Operator){
+
+    console.log('Assessment')
+    Num2 = parseFloat(screenLwr.textContent)
+    Second_Operator = Operator
+
+    screenLwr.textContent = Cal_Result(Num1,Num2,First_Operator)
+    screenUpr.textContent = `${Num1} ${First_Operator} ${Num2} ${Second_Operator}`
+
+    Num1 = screenLwr.textContent
+    Num2 = undefined
+    
+
+    
+}
+
+
+function Cal_Result(num1, num2, operator){
+
+    switch(operator){
+        case "+":
+            return add(num1,num2)
+        
+        case "-":
+            return sub(num1,num2)
+
+        
+        case "*":
+            return multiply(num1,num2)
+        
+        case "/":
+            return add(num1,num2)
+        
+        case "=":
+            return divide(num1,num2)
+        
+
+    }
+
+}
+
+
+
+
+Num_Buttons.forEach( button => {
+    button.addEventListener('click', ()=> AddNum(button.textContent))
+})
+
+Operator_Buttons.forEach( button => {
+    button.addEventListener('click', ()=> GetOperator(button.textContent))
+})
