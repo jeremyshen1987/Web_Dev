dc973fc1b038f996890335b26eab34144c19c20e
Jeremy Shen <jeremyshen1987@gmail.com>
2022-04-03 12:45:08 -0600

    main func can now take variable args. Bug fix: lengthy word from dictionary end the loop prematurely, causing single word ("it","i") miss the count

--- Ruby/02 Sub Strings.rb
+++ Ruby/02 Sub Strings.rb
@@ -1,13 +1,12 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-kw = "How's it going?"
-kw_arr = kw.split('')
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+sorted_dictionary = dictionary.sort_by(&:length)
 
 def count(kw_arr, word, word_count)
 
     i = 0
 
-    while i < (kw_arr.length - word.length) do
+    while i <= (kw_arr.length - word.length) do
 
         constructed_word = kw_arr[i...(i + word.length)].join('')
 
@@ -26,7 +25,6 @@ def count(kw_arr, word, word_count)
             puts "condition not met"
 
         end
-        
 
         i += 1
     end
@@ -36,17 +34,29 @@ def count(kw_arr, word, word_count)
 end
 
 
+def main(*args, sorted_dictionary)
 
-word_count = {}
+    word_count = {}
+    args.each do |arg|
 
-dictionary.each do |word|
+        puts "!!!!!!!!!!!!!!!!!#{arg}!!!!!!!!!!!!!!!"
 
-    count(kw_arr, word, word_count)
+        kw_arr = arg.downcase.split('')  
+
+        sorted_dictionary.each do |word|
+
+            puts "word from sorted_dictionary is #{word}"
+            count(kw_arr, word, word_count)
+            
+        end
+        
+    end
     
+    puts word_count
 end
 
+main("Howdy partner, sit down! How's it going?", "horn it sit below",sorted_dictionary)
 
-puts word_count
 
 
 
