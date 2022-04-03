b5d8b4842271775afaeef22e2b9dd545440994ab
Jeremy Shen <jeremyshen1987@gmail.com>
2022-04-02 21:32:30 -0600

    project complete

--- Ruby/01 Caesar cipher.rb
+++ Ruby/01 Caesar cipher.rb
@@ -1,10 +1,14 @@
-alphabat = ('a'..'z').to_a
 
-str = "What a string!"
+def cipher_text(str, shift_factor)
+
+alphabat_down = ('a'..'z').to_a
+alphabat_Uppr = ('A'..'Z').to_a
+alphabat = alphabat_down + alphabat_Uppr
+
+
 
 str_arr = str.split('')
 
-puts str_arr
 
 str_arr_num= str_arr.map{ |letter|
     if alphabat.include?(letter)
@@ -12,7 +16,36 @@ str_arr_num= str_arr.map{ |letter|
     else
         letter
     end
-    
 }
 
 
+
+
+shifted_str_num = str_arr_num.map{ |num|
+    if num.class == Integer
+        if (num < 26 && num + shift_factor > 26) || (num > 26 && num + shift_factor > 52)
+            num = num + shift_factor - 26
+        else
+            num = num + shift_factor
+        end
+
+    else
+        num
+    end
+}
+
+shifted_arr = shifted_str_num.map { |num|
+    if num.class == Integer
+        num = alphabat[num]
+    else 
+        num
+    end
+}
+
+cipher_text = shifted_arr.join('')
+
+puts cipher_text
+
+end
+
+cipher_text('What a string!', 5)
