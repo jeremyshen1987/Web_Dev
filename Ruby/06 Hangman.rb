b51182e3a3990f29cffc3f42ba5880acae92f078
Jeremy Shen <jeremyshen1987@gmail.com>
2022-04-10 10:35:50 -0600

    script can now load words from a txt file. user input are now checked against past guess

--- Ruby/06 Hangman.rb
+++ Ruby/06 Hangman.rb
@@ -1,13 +1,39 @@
+class Words
+  
+    attr_accessor :words
+  
+    def initialize
+  
+      @words = []
+    end
+  
+    def load_words
+  
+      line = File.readlines('google-10000-english-no-swears.txt', chomp:true).each do |li|
+        words.push(li) unless li.length < 5 || li.length > 12 || words.include?(li)
+      end
+    end
+  
+    def rand_word
+      load_words
+      return words[rand(100)]
+    end
+  
+  end
+  
+
+
 class Game
 
-    attr_accessor :secret_word, :masked_word, :missed_word
+    attr_accessor :secret_word, :masked_word, :missed_guess, :correct_guess
     attr_reader :attempts
 
 
     def initialize(secret_word)
         @secret_word = secret_word
         @masked_word = ""
-        @missed_word = []
+        @missed_guess = []
+        @correct_guess = []
         @attempts = 15
     end
 
@@ -34,11 +60,11 @@ class Game
     def validate(guess)
     
         if secret_word.include?(guess)
+            correct_guess.push(guess) unless correct_guess.include?(guess)
             replace_word(guess)
         else
             puts "Oops, that's the wrong guess!\n\n"
-            missed_word.push(guess) if !missed_word.include?(guess)
-            puts "Miss: #{missed_word}" 
+            missed_guess.push(guess) if !missed_guess.include?(guess) 
             sleep(1)
         end
     end
@@ -53,7 +79,7 @@ class Game
              
         elsif attempts == 0
 
-            puts "Game over. You didn't get the whole word"
+            puts "No attempts left. The word is: #{secret_word}"
             return true
         end
     
@@ -64,9 +90,13 @@ class Game
         
         puts "Which letter would yo like to guess?"
         letter = gets.chomp
-        return letter if letter.length == 1 && letter =~ /[A-Za-z]/
-
-        puts "ONE LETTER only! Try again: "
+        return letter if letter.length == 1 && letter =~ /[a-z]/ && !correct_guess.include?(letter) && !missed_guess.include?(letter)
+        if correct_guess.include?(letter) || missed_guess.include?(letter)
+            puts "\nYou already guessed that.\n\n"
+        else
+            puts "ONE LOWERECASE LETTER only! Try again: "
+        end
+        
         player_input
     end
 
@@ -75,8 +105,8 @@ class Game
         create_masked_word
 
         until game_over
-
-            puts "\n###########################\n\nyou have #{attempts} attempts left\n\n"
+            puts "\nMissed: #{missed_guess}" unless missed_guess.empty?
+            puts "###########################\n\nyou have #{attempts} attempts left\n\n"
             puts "#{masked_word} \n\n"
             guess = player_input
             validate(guess)
@@ -93,5 +123,7 @@ class Game
 
 end
 
-play = Game.new('secret')
+
+get_words = Words.new
+play = Game.new(get_words.rand_word)
 play.start_game
