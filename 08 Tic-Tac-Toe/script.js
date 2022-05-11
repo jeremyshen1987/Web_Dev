e024b47bfb99eeae1217579afa733c0350a9db98
Jeremy Shen <jeremyshen1987@gmail.com>
2022-05-10 21:59:07 -0600

    switch player, announce winner

--- 08 Tic-Tac-Toe/script.js
+++ 08 Tic-Tac-Toe/script.js
@@ -12,20 +12,25 @@ const GameBoard = (function(){
             _board[num] = marker
         },
 
-        win_condition: function(){
+        win: function(Current_Player){
+
+            console.log(`${Current_Player.getName()} win!`)
+        },
+
+        win_condition: function(Current_Player){
             for(i = 0; i < 7; i += 3){
                 if(_board[i] == _board[i + 1] && _board[i + 1] == _board[i + 2]){
-                    win()
+                    this.win(Current_Player)
                 }
             }
 
             for(i = 0; i < 3; i += 1){
                 if(_board[i] == _board[i + 3] && _board[i + 3] == _board[i + 6]){
-                    win()
+                    this.win(Current_Player)
                 }
             }
             if((_board[0] == _board[4] && _board[4] == _board[8]) || (_board[2] == _board[4] && _board[4] == _board[6])){
-                win()
+                this.win(Current_Player)
             }
         },
 
@@ -37,6 +42,7 @@ const GameBoard = (function(){
     }
     
 })()
+
 
 const player = (name, marker) => {
 
@@ -50,25 +56,38 @@ const player = (name, marker) => {
 }
 
 
-const control = function(){
+const control = (function(){
 
-    // GameBoard.place_marker(3, 'X')
-
-    // GameBoard.show_board()
-
-}
+    const p1 = player('Player_1','X')
+    const p2 = player('Player_2', 'O')
 
 
-const buttons = document.querySelectorAll('.grid')
+    const players = [p1, p2]
+
+    let Current_Player = p1
+
+    const buttons = document.querySelectorAll('.grid')
 
 
-buttons.forEach((button) => {
-    button.onclick = function(){
-        
-        this.textContent = 'X'
+    buttons.forEach((button) => {
 
-        position = this.dataset.key
-        GameBoard.place_marker(position, 'X')
+        button.onclick = function(){
+            
+            this.textContent = Current_Player.getMarker()
 
-    }
-});
+            position = this.dataset.key
+            GameBoard.place_marker(position, Current_Player.getMarker())
+
+
+            GameBoard.win_condition(Current_Player)
+
+            Current_Player = players[1 - players.indexOf(Current_Player)]
+
+        }
+
+    });
+
+
+})(player, GameBoard)
+
+
